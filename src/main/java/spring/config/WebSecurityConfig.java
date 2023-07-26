4c52e4d7c721233a73230c71d47884264620de77
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-07-26 11:58:52 +0200

    added some commits about Spring boot annotations

--- src/main/java/spring/config/WebSecurityConfig.java
+++ src/main/java/spring/config/WebSecurityConfig.java
@@ -10,7 +10,22 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+//используются для настройки и включения безопасности веб-приложения.
+
+/**
+ * @Configuration указывает, что класс является конфигурационным классом Spring,
+ * который содержит одну или несколько методов, помеченных аннотацией @Bean.
+ * Методы, помеченные аннотацией @Bean, создают и конфигурируют объекты,
+ * которые затем добавляются в контейнер Spring и могут использоваться в других частях приложения.
+ */
 @Configuration
+/**
+ * @EnableWebSecurity включает поддержку веб-безопасности в приложении Spring.
+ * Он активирует класс WebSecurityConfigurerAdapter, который позволяет настроить
+ * безопасность приложения путем переопределения его методов. Например, вы можете настроить
+ * аутентификацию и авторизацию пользователей, указать правила доступа к конечным точкам (URL),
+ * настроить кросс-доменную аутентификацию и т. д.
+ */
 @EnableWebSecurity
 public class WebSecurityConfig {
 
